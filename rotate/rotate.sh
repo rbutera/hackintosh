2111913fc00e9cedaa5b0d74d07c0abc4cf5d7e1
Rai <rai@rbutera.com>
2018-11-13 18:56:49 +0000

    updated for new build

--- rotate/rotate.sh
+++ rotate/rotate.sh
@@ -1,4 +1,4 @@
-/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0xb432679 -r 0
-/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0x745f94c8 -r 0
-/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0xb432679 -r 180
-/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0x745f94c8 -r 90
+/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0xb43267b -r 0
+/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0x745f94ca -r 0
+/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0xb43267b -r 180
+/Users/rai/hackintosh/rotate/fb-rotate/fb-rotate -d 0x745f94ca -r 90
